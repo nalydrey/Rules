0419328bc473ac4961f1ecd49bea2727ed02ead2
Alex <nalydrey@gmail.com>
2022-10-16 21:25:32 +0300

    corectone

--- Logic.js
+++ Logic.js
@@ -4,12 +4,8 @@ console.dir(visibleContent);
 visibleContent.forEach(element => {   
     element.addEventListener('click', function(){
         let statusDisplay = getComputedStyle(element.nextElementSibling, null).display
-        console.log(getComputedStyle(element.nextElementSibling, null).display)
-        if (statusDisplay === 'none')
-        {
-            element.nextElementSibling.style.display = 'block';
-        }
-        else {element.nextElementSibling.removeAttribute('style')}
+        console.log(getComputedStyle(element.nextElementSibling, null).display);     
+        element.nextElementSibling.classList.toggle('hideContent');      
     })
 })
 
