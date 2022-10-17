79b55a6583a9188abcb1aaa449bb8ebd3dfa6d4a
Alex <nalydrey@gmail.com>
2022-10-17 15:07:23 +0300

    horisontal scroll

--- Logic.js
+++ Logic.js
@@ -2,13 +2,14 @@ let visibleContent = document.querySelectorAll('.chapter');
 console.dir(visibleContent);
 
 visibleContent.forEach(element => {   
-    element.addEventListener('click', function(){
-        let statusDisplay = getComputedStyle(element.nextElementSibling, null).display
-        console.log(getComputedStyle(element.nextElementSibling, null).display);     
-        element.nextElementSibling.classList.toggle('hideContent');      
-    })
+    element.addEventListener('click', ShowNextElement);
 })
 
+function ShowNextElement(){   
+    let nextElem = this.nextElementSibling
+    nextElem.classList.toggle('hideContent'); 
+    
 
+}
 
 
